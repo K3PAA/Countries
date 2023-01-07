f65ce5ef261656f5b0b0025d983873cdfab36fd0
K3PAA <jkepa00@gmail.com>
2023-01-07 11:04:09 +0100

    CTA Buttons

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function CTA() {
-  return (
+function CTA({ code }) {
+  const [fullName, setFullName] = useState([])
+
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/alpha/${code}`)
+      .then((data) => data.json())
+      .then((data) => setFullName(data))
+  }, [])
+
+  return fullName[0] ? (
     <div>
-      <h1>CTA</h1>
+      <h1 onClick={() => console.log(90)}>{fullName[0].name.common}</h1>
     </div>
+  ) : (
+    <h1>Fetching data</h1>
   )
 }
 
--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 import { Link, useParams } from 'react-router-dom'
+import CTA from './CTA'
 
 function Detail() {
   const { id } = useParams()
@@ -14,8 +15,6 @@ function Detail() {
       .catch((err) => console.log(err))
   }, [])
 
-  console.log(country)
-
   return country ? (
     <div>
       {/* Left Side */}
@@ -26,7 +25,20 @@ function Detail() {
       {/* End Left Side */}
       {/* Right Side */}
       <section>
-        <p>Native Name: {country.nativeName}</p>
+        <h1>{country.name.common}</h1>
+        <p>Native Name: {Object.values(country.name.nativeName)[0].official}</p>
+        <p>Population: {country.population}</p>
+        <p>Region: {country.region}</p>
+        <p>Subregion: {country.subregion}</p>
+        {country.capital && <p>Capital: {country.capital[0]} </p>}
+        <p>Top Level Domain: {country.tld.join(' / ')}</p>
+        <p>Currencies: {Object.values(country.currencies)[0].name}</p>
+        {/* <p>Languages: {country.languages}</p> */}
+
+        {country.borders &&
+          country.borders.map((border, index) => {
+            return <CTA key={index} code={border} />
+          })}
       </section>
       {/* End Right Side */}
     </div>
