90ea14b41fdf5ceffdd9bbd1ed2d30b8e72ac31d
K3PAA <jkepa00@gmail.com>
2023-01-08 18:20:38 +0100

    Adding dist

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home'
-import CTA from './CTA'
-import Detail from './Detail'
-import FilterSection from './FilterSection'
-import SingleCard from './SingleCard'
-import Error from './Error.jsx'
-import SharedLayout from './SharedLayout'
-
-export { Home, CTA, Detail, FilterSection, SingleCard, Error, SharedLayout }
--- src/style.js
+++
@@ -1,9 +0,0 @@
-const styles = {
-  boxWidth: 'xl:max-w-[1280px] w-full',
-  typography: 'font-nunito font-normal',
-  paragraph: 'font-nunito font-normal text-[16px] text-lightest',
-}
-
-const layouts = {}
-
-export default styles
